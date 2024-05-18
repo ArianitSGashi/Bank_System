6d896568edb6924c2145d132410b54b7b508261a
Arianit Gashi <gashiarianit2003@gmail.com>
2024-05-18 13:15:41 +0200

    BSS-1 added a basic structure of spring boot app

---
+++ src/main/java/com/springboot/banksystem/BanksystemApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.banksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanksystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanksystemApplication.class, args);
+	}
+
+}
